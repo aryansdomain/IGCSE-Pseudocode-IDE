10688435b63e4c92c70aa0b0b981e0e497d4d694
Dreamer1297 <ascoder1248@gmail.com>
2025-09-24 15:44:06 +0530

    fix comments

--- src/terminal/consoleOutput.js
+++ src/terminal/consoleOutput.js
@@ -1,4 +1,5 @@
 export function createConsoleOutput(terminal) {
+    
     const write = (text, color = null) => {
         if (color) terminal.write(`\x1b[${color}m${text}\x1b[0m`);
         else terminal.write(text);
@@ -12,13 +13,13 @@ export function createConsoleOutput(terminal) {
         lnprint:   (t = '', c = null) => write(`\r\n${t}`, c),
         lnprintln: (t = '', c = null) => write(`\r\n${t}\r\n`, c),
 
-        // errors (red)
+        // error (red)
         err:     (t = '') => write(t, '31'),
         errln:   (t = '') => write(`${t}\r\n`, '31'),
         lnerr:   (t = '') => write(`\r\n${t}`, '31'),
         lnerrln: (t = '') => write(`\r\n${t}\r\n`, '31'),
 
-        // warnings (italics + yellow)
+        // warning (italics + yellow)
         warn:     (t = '') => write(`\x1b[3m\x1b[33m${t}\x1b[0m`),
         warnln:   (t = '') => write(`\x1b[3m\x1b[33m${t}\x1b[0m\r\n`),
         lnwarn:   (t = '') => write(`\r\n\x1b[3m\x1b[33m${t}\x1b[0m`),
