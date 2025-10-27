47ea5b67d706d93d52371712866fcadfff28e550
Dreamer1297 <ascoder1248@gmail.com>
2025-10-27 17:14:41 +0530

    add https://igcse-ide.com as a valid url for hosting

--- src/report/buildIssue.js
+++ src/report/buildIssue.js
@@ -1,63 +1,6 @@
 import { isLightMode } from '../ui/modeCtrl.js';
 import { formatJSError } from './catchError.js';
 
-// config
-const GH_OWNER = "aryansdomain";
-const GH_REPO  = "IGCSE-Pseudocode-IDE";
-
-// create Git Issues URL with error template
-function buildIssueURL() {
-    const base = `https://github.com/${GH_OWNER}/${GH_REPO}/issues/new`;
-    const params = new URLSearchParams({
-        //template: "issue_report.yml",
-        labels: "issue",
-        title: "",
-    });
-
-    // context
-    const ua  = navigator.userAgent;
-    const ver = window.__APP_VERSION ? `\nApp: ${window.__APP_VERSION}` : "";
-    const lastJSError = formatJSError(window.__lastError);
-    const lastIDEError = window.__lastIDEError || "None";
-
-    // base template
-    const body = [
-        "Type (UI issue, runtime error, formatting bug, etc.): ",
-        "",
-        "Steps to reproduce:",
-        "1. ",
-        "2. ",
-        "3. ",
-        "",
-        "Expected result:",
-        "",
-        "",
-        "",
-        "Actual result:",
-        "",
-        "",
-        "",
-        "Additional information:",
-        "",
-        "",
-        "",
-        "------------------------ PREFILLED INFORMATION ------------------------",
-        "",
-        `UA: ${ua}\n`,
-        ver ? `Version: ${ver}\n\n` : "",
-        "Last JavaScript Error:",
-        "",
-        lastJSError,
-        "",
-        "Last IDE Error:",
-        "",
-        lastIDEError,
-    ].join("\n");
-
-    params.set("body", body);
-    return `${base}?${params.toString()}`;
-}
-
 // open issue report page
 function wireIssueButton() {
     const el = document.getElementById("issue-report-btn");
@@ -67,12 +10,7 @@ function wireIssueButton() {
         e.preventDefault();
         
         // build report.html URL and fill data
-        let reportPath = "";
-
-        const isLocalHost = location.hostname === 'localhost' || location.hostname === '127.0.0.1';
-        if (!isLocalHost) reportPath = `${GH_REPO}`;
-        reportPath += "/src/report/report.html";
-        
+        const reportPath = "/src/report/report.html";
         const url = new URL(reportPath, location.origin);
         
         // pass info to url
