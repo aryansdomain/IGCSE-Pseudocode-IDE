f1cb2685aebabeb3ffb294cfbe5de6be2897a3c0
Dreamer1297 <ascoder1248@gmail.com>
2026-05-18 21:34:00 +0530

    implement custom backspace logic

--- src/editor/editor.js
+++ src/editor/editor.js
@@ -33,6 +33,40 @@ export function initEditor({
         setCode(format(getCode(), getTabSize()));
     }
 
+    // override backspace command
+    // on a whitespace-only line, backspace moves cursor to previous indent level
+    aceEditor.commands.addCommand({
+        name: 'backspace',
+        bindKey: { win: 'Backspace', mac: 'Backspace' },
+        exec(ed) {
+            const session = ed.getSession();
+            const cursor  = ed.getCursorPosition();
+            const line = session.getLine(cursor.row);
+
+            if (line.length > 0 && /^\s+$/.test(line)) { // only whitespace
+                const currentIndent = line.length;
+ 
+                // find last indent
+                for (let i = cursor.row - 1; i >= 0; i--) {
+                    const prevLine = session.getLine(i);
+                    if (/^\s*$/.test(prevLine)) continue; // only whitespace
+
+                    const prevIndent = prevLine.match(/^ */)[0].length; // indent level
+                    if (prevIndent < currentIndent) {
+                        // move to previous indent
+                        const Range = ace.require('ace/range').Range;
+                        const startToCursor = new Range(cursor.row, 0, cursor.row, currentIndent);
+                        session.replace(startToCursor, ' '.repeat(prevIndent));
+
+                        ed.moveCursorTo(cursor.row, prevIndent);
+                        return;
+                    }
+                }
+            }
+            ed.remove('left');
+        }
+    });
+
     const editor = { getCode, setCode, setTab, getTabSize, formatCode };
     return { aceEditor, editor };
 }
